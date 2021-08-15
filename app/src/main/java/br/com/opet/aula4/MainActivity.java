a00c17487141fb2a88f23d952b0b6a6e013e2a82
jibalbinot <jibalbinot@gmail.com>
2021-08-15 11:17:56 -0300

    Envio de e-mail com intents

--- app/src/main/java/br/com/opet/aula4/MainActivity.java
+++ app/src/main/java/br/com/opet/aula4/MainActivity.java
@@ -2,8 +2,12 @@ package br.com.opet.aula4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,11 +16,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("MAINACTIVITY","criou a aplicação");
-        chamarIntent();
-
     }
 
-    public void chamarIntent(){
-        Log.i("MAINACTIVITY","entrei na função");
+    public void chamarIntent(View v){
+        Intent it = new Intent(Intent.ACTION_SEND);
+        it.setType("text/plain");
+        it.putExtra(Intent.EXTRA_EMAIL, new String[] {"coordenadoraTDS@uniopet.edu.br","k@gmail.com"});
+        it.putExtra(Intent.EXTRA_SUBJECT,"Aula sobre intents explícitas");
+        it.putExtra(Intent.EXTRA_TEXT,"Olá coordenadora, estou escrevendo esse email para teste");
+
+        try {
+            startActivity(it.createChooser(it,"Enviar e-mail por: "));
+        }catch (ActivityNotFoundException e){
+            e.printStackTrace();
+        }
     }
 }
